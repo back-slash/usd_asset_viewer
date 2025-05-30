581b47b286a53785910a4db603ed1d65bd2bf26c
back-slash <sumnall.nik@gmail.com>
2025-05-30 10:51:43 -0600

    Build improvements..

--- build_run.py
+++ build_run.py
@@ -9,13 +9,15 @@ import subprocess
 import sys
 import argparse
 import tempfile
+import pprint as pp
 
+#####################################################################################################################################
+USD_PATH = "" # USE IF MANUALLY BUILDING OR ALREADY HAVE USD BUILT
 #####################################################################################################################################
 parser = argparse.ArgumentParser()
 parser.add_argument("--run", action="store_true")
 args = parser.parse_args()
 #####################################################################################################################################
-
 os_type = os.name
 if os_type == 'nt': python_name = "python"
 else: python_name = "python3"
@@ -23,7 +25,6 @@ build_directory = "build"
 cmake_cache = os.path.join(build_directory, "CMakeCache.txt")
 venv_directory = os.path.join(os.path.dirname(__file__), ".venv")
 requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
-
 #####################################################################################################################################
 
 def find_vs_path():
@@ -76,59 +77,62 @@ def build_usd_module():
         if not vs_path:
             print("Visual Studio not found. Please install Visual Studio with C++ development tools.")
             return False
-        print(f"Setting up MSVC environment...")
         command_list.insert(0, f'call "{vs_path}" x64\n',)
         run_python_command(command_list)
         return True
     else:
-        command_list.insert(1, f'cd {usd_path}\n')        
-        command_list.insert(2, f'{sys.executable} venv {os.path.join(usd_path, ".venv")}\n')
-        command_list.insert(3, f'source {os.path.join(usd_path, ".venv", "bin", "activate")}\n')
-        command_list.append(4, f'pip install PySide6 PyOpenGL')
+        command_list.insert(0, f'cd {usd_path}\n')        
+        command_list.insert(1, f'{sys.executable} -m venv {os.path.join(usd_path, ".venv")}\n')
+        command_list.insert(2, f'. {os.path.join(usd_path, ".venv", "bin", "activate")}\n')
+        command_list.insert(3, f'pip install PySide6 PyOpenGL')
+        command_list.append(f'rm -f .venv\n')
         run_python_command(command_list, venv=False)
 
 
 
 def init_submodules():
-    print("Initializing git submodules...")
+    special_print("Initializing git submodules...")
     try:
         subprocess.run(["git", "submodule", "update", "--init", "--recursive"], check=True, cwd=os.path.dirname(__file__))
-        print("Sub-modules initialized successfully...")
+        special_print("Sub-modules initialized successfully...")
     except subprocess.CalledProcessError:
-        print("Warning: git not found")
+        special_print("Warning: git not found")
 
 
 def check_usd_build():
+    if os.path.exists(USD_PATH):
+        special_print("USD build found at:", USD_PATH)
+        return True
     include_path = os.path.join(os.path.dirname(__file__), "external", "OpenUSD_.flag")
     return os.path.exists(include_path)
     
 
 def create_build():
     if not os.path.isfile(cmake_cache):
-        print(f"Generating CMake files in {build_directory}...")
+        special_print(f"Generating CMake files in {build_directory}...")
         if not os.path.isdir(build_directory):
             os.makedirs(build_directory)
         subprocess.check_call(["cmake", "-S", ".", "-B", build_directory])
 
 
 def run_build_process():
-    print(f"Running CMake in {build_directory}...")
+    special_print(f"Running CMake in {build_directory}...")
     subprocess.check_call(["cmake", "--build", build_directory, "--config", "Release"])
 
 
 def setup_virtual_environment():
     if not os.path.isdir(venv_directory):
-        print("Creating virtual environment...")
+        special_print("Creating virtual environment...")
         subprocess.check_call([sys.executable, "-m", "venv", venv_directory])
 
 
 def install_python_dependencies():
-    print("Check/Install Python requirements...")
+    special_print("Check/Install Python requirements...")
     run_python_command([f'{python_name} -m pip install -r "{requirements_file}"'])
 
 
 def run():
-    print("Running the USD Asset Viewer...")
+    special_print("Running the USD Asset Viewer...")
     run_python_command([f"{python_name} build_run.py --run"])
 
 
@@ -148,6 +152,17 @@ def build_run():
     run()
 
 
+def special_print(printable):
+    """
+    Print script notes
+    """
+    # Print each pound sign in a different ANSI color
+    colors = ['\033[91m', '\033[92m', '\033[94m']  # Red, Green, Blue
+    reset = '\033[0m'
+    orange = '\033[93m'
+    print(' '.join(f"{color}#{reset}" for color in colors), f"{orange}{printable}{reset}",)
+
+
 #####################################################################################################################################
 
 
@@ -155,6 +170,8 @@ if not args.run:
     build_run()
 
 else:
-    sys.path.append(os.path.join(os.path.dirname(__file__), "external", "OpenUSD_", "lib", "python"))
+    if not USD_PATH:
+        USD_PATH = os.path.join(os.path.dirname(__file__), "external", "OpenUSD_", "lib", "python")
+    sys.path.append(USD_PATH)
     import tool.base_tool as base_tool
     base_tool.USDAssetViewer()
